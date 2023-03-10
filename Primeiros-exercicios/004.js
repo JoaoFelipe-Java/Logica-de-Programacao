535080cda295655e84c2c7f5faba47f91f80e308
JoaoFelipe-Java <joaofelipeoliveiradejesus@gmail.com>
2023-03-10 18:22:47 -0300

    commit

---
+++ Primeiros-exercicios/004.js
@@ -0,0 +1,64 @@
+//Reescreva as instruções abaixo com o mínimo de parênteses possível, mas sem alterar o resultado
+
+/*
+A 6*(3+2) 
+F (6/3)+(8/2)
+B 2+(6*(3+2)) 
+G ((3+(8/2))*4)+(3*2)
+C 2+(3*6)/(2+4) 
+H (6*(3*3)+6)-10
+D 2*(8/(3+1)) 
+I (((10*8)+3)*9)
+E 3+(16-2)/(2*(9-2)) 
+J ((-12)*(-4))+(3*(-4))
+*/
+
+// Resutado A=30 calculo inicial 6*(3+2)
+console.log('Resultado da Letra A')
+let A = 6*(3+2);
+console.log(A)
+
+// Resutado B=32 calculo inicial 2+(6*(3+2))
+console.log('Resultado da Letra B')
+let B = 2+6*(3+2);
+console.log(B)
+
+// Resutado C=5 calculo inicial 2+(6*(3+2))
+console.log('Resultado da Letra C')
+let C = 2+3*6/(2+4);
+console.log(C)
+
+// Resutado D=4 calculo inicial 2*(8/(3+1))
+console.log('Resultado da Letra D')
+let D = 2*8/(3+1);
+console.log(D);
+
+// Resutado E=4 calculo inicial 3+(16-2)/(2*(9-2))
+console.log('Resultado da Letra E')
+let E = 3+(16-2)/(2*(9-2));
+console.log(E);
+
+// Resutado F=6 calculo inicial (6/3)+(8/2)
+console.log('Resultado da Letra F')
+let F = 6/3+8/2;
+console.log(F);
+
+// Resutado G=30 calculo inicial ((3+(8/2))*4)+(3*2)
+console.log('Resultado da Letra G')
+let G = (3+8/2)*4+3*2;
+console.log(A)
+
+// Resutado H=50 calculo inicial (6*(3*3)+6)-10
+console.log('Resultado da Letra H')
+let H = 6*(3*3)+6-10;
+console.log(H);
+
+// Resutado I=747 calculo inicial (((10*8)+3)*9)
+console.log('Resultado da Letra I')
+let I = (10*8+3)*9;
+console.log(I);
+
+// Resutado J=36 calculo inicial ((-12)*(-4))+(3*(-4))
+console.log('Resultado da Letra J')
+let J = -12*-4+3*-4;
+console.log(J);
---
+++ README.md
@@ -0,0 +1,2 @@
+# Logica-de-Programacao
+Estudo de lógica de programação no VisualG e javascript.
